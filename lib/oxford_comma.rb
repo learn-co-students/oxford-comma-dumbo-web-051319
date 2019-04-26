528ceb2b1f6d0447750ac8e2922a0bec735cf1ec
Rachel R <rrrachelrath@gmail.com>
2019-04-26 12:42:41 -0400

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    return array.first
+  elsif array.length == 2
+    return "#{array.first} and #{array.last}"
+  else last_word = array.pop
+    return "#{array.join(", ")}, and #{last_word}"
+  end
 end
