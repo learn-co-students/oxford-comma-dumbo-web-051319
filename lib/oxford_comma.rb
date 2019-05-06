9c82e598d3ecca3cf1c4aac4ed09b34397adf6cd
Joshua Suskin <jsuskin85@gmail.com>
2019-05-06 22:36:35 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,8 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    return array[0]
+  else
+    array[-1].insert(0, "and ")
+    return array.size == 2 ? array.join(" ") : array.join(", ")
+  end
 end
